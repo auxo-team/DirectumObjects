e489de720e2ab88d53c4ecad204604767929f1a0
DirectumRXUser <directumrxuser@c.ru>
2025-07-04 16:41:59 +0300

    Логика FillEntitiesProperties() вынесена в АО

--- Auxo.Common/Auxo.Common.Server/ModuleAsyncHandlers.cs
+++ Auxo.Common/Auxo.Common.Server/ModuleAsyncHandlers.cs
@@ -9,5 +9,18 @@ namespace Auxo.Common.Server
   public class ModuleAsyncHandlers
   {
 
+    /// <summary>
+    /// Актуализировать свойства сущности.
+    /// </summary>
+    /// <param name="args">Аргументы обработчика.</param>
+    public virtual void UpdateEntityProperties(Auxo.Common.Server.AsyncHandlerInvokeArgs.UpdateEntityPropertiesInvokeArgs args)
+    {
+      var postfix = string.Format("UpdateEntityProperties_{0}", args.ObjTypeId);
+      Logger.WithLogger(postfix).Debug(string.Format("Handler starting. ObjTypeId = {0}", args.ObjTypeId));
+      
+      
+      Logger.WithLogger(postfix).Debug("Handler complite");
+    }
+
   }
 }
